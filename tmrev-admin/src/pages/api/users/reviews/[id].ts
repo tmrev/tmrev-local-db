d520329352e4e2c94d573d722891fde72ff81684
Kegen Guyll <guyllkegen@gmail.com>
2025-11-07 22:08:01 -0600

    fix: update reviews retrieval to use userId and fetch from external API

--- tmrev-admin/src/pages/api/users/reviews/[id].ts
+++ tmrev-admin/src/pages/api/users/reviews/[id].ts
@@ -1,4 +1,6 @@
+import { createIdTokenfromCustomToken } from '@/functions/exchangeCustomToken';
 import { initFirebase } from '@/functions/initFirebase';
+import axios from 'axios';
 import { MongoClient, ObjectId } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
@@ -13,17 +15,23 @@ export default async function handler(
   await client.connect();
   const { id } = req.query
   if(req.method === 'GET') {
+    initFirebase()
+
     // return all users from db
     const dbUser = client.db('tmrev').collection('users')
-    const dbReviews = client.db('tmrev').collection('reviews')
     const _id = new ObjectId(id as string)
 
     const user = await dbUser.findOne({_id});
-    const reviews = await dbReviews.find({userId: user?.uuid}).toArray();
+    const token = await createIdTokenfromCustomToken(user?.uuid)
+    const reviews = await axios.get(`${process.env.TMREV_API_BASE_URL}/review/user/${user?.uuid}?pageSize=1000`, {
+      headers: {
+        'Authorization': token
+      }
+    })
 
     res.status(200).json({result: {
       user,
-      reviews
+      reviews: reviews.data?.results || []
     }})
     client.close(true)
   }
